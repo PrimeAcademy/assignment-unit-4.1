bd5dc0bd1d0539e048e5f7d7c41c346b94637586
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-04-18 04:45:04 -0500

    I'm stuck on questions 6 and 7

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -38,28 +38,33 @@ console.log (multiplyThree (2, 3, 4 ));
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+  else {
+    return false;
+  }
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log( 'The number 3 is Positive - should say true', isPositive(3) );
+console.log( 'The number 0 is Positive - should say false', isPositive(0) );
+console.log( 'The number -3 is Positive - should say false', isPositive(-3) );
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
+function getLast( array ) 
 
-}
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
+  let array = [5, 7, 12, 20, 33];
+  while(array){
+    if(value = 20)
+  }
+  }
 }
 
 // ----------------------
